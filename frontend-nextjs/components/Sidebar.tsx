1701e5fefd9cc3b049ddae2bf1b3aeb9b562fc97
Sarwar Sami <samixshady22@gmail.com>
2026-02-02 10:30:12 +0600

    Removed Old HTML

--- frontend-nextjs/components/Sidebar.tsx
+++ frontend-nextjs/components/Sidebar.tsx
@@ -73,10 +73,10 @@ export default function Sidebar({ isMobileOpen, setIsMobileOpen }: SidebarProps)
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
               </svg>
               <div className="text-4xl font-bold relative overflow-hidden h-10 flex items-center">
-                <span className="bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-400 dark:to-pink-400 bg-clip-text text-transparent">
+                <span className="relative z-10 bg-gradient-to-r from-purple-600 via-pink-600 to-purple-600 dark:from-purple-400 dark:via-pink-400 dark:to-purple-400 bg-clip-text text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
                   NavFlow
                 </span>
-                <div className="aurora absolute inset-0">
+                <div className="aurora absolute inset-0 hidden dark:block">
                   <div className="aurora__item aurora__item--1"></div>
                   <div className="aurora__item aurora__item--2"></div>
                   <div className="aurora__item aurora__item--3"></div>
